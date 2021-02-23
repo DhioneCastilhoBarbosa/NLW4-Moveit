7414e224acdb7d9b770117588af43b6e2e55a219
DhioneCastilhoBarbosa <dhione.cb@hotmail.com>
2021-02-23 15:21:00 -0300

    primeira-aula

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+    return (
+        <head className="experience-bar">
+          <span>0 xp</span>
+          <div>
+            <div style={{width:'50%'}}/> 
+            <span 
+            className="current-experiencie" style={{left:'50%'}}>
+                300xp
+                </span>
+          </div>
+          <span>600 xp</span>    
+        </head>
+    );
+}
